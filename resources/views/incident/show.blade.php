b966796d250d62b4e042dc5bd39ea0851810d37b
MGhufranKhalil <ghufranrana95@gmail.com>
2024-06-12 11:44:40 +0500

    fixing

--- resources/views/incident/show.blade.php
+++ resources/views/incident/show.blade.php
@@ -16,22 +16,24 @@
         </nav>
         <div class="card">
             <div class="card-body">
-                <div class="col-md-6">
-                    @if (auth()->user()->hasRole('super-admin') && auth()->user()->company_id == 0)
-                        <h5>{{ $incident->company->name ?? ' - ' }}</h5>
-                    @endif
-                    <h5>Employee: {{ $incident->employee->name }}</h5>
-                    <h5>Location: {{ $incident->location }}</h5>
-                    <h5>Date: {{ \Carbon\Carbon::parse($incident->date)->format('m/d/Y') }}</h5>
-                    <h5>Time: {{ $incident->time }}</h5>
-                    <h5>Description: {{ $incident->details }}</h5>
-                </div>
-                <div class="col-md-6 float-right">
-                    @foreach ($incident->images as $image)
-                        <a href="{{ asset($image->image) }}"  target="_blank">
-                            <img src="{{ asset($image->image) }}" class="img-fluid" width="100">
-                        </a>
-                    @endforeach
+                <div class="row g-3">
+                    <div class="col-md-6">
+                        @if (auth()->user()->hasRole('super-admin') && auth()->user()->company_id == 0)
+                            <h5>{{ $incident->company->name ?? ' - ' }}</h5>
+                        @endif
+                        <h5>Employee: {{ $incident->employee->name }}</h5>
+                        <h5>Location: {{ $incident->location }}</h5>
+                        <h5>Date: {{ \Carbon\Carbon::parse($incident->date)->format('m/d/Y') }}</h5>
+                        <h5>Time: {{ $incident->time }}</h5>
+                        <h5>Description: {{ $incident->details }}</h5>
+                    </div>
+                    <div class="col-md-6 float-right">
+                        @foreach ($incident->images as $image)
+                            <a href="{{ asset($image->image) }}"  target="_blank">
+                                <img src="{{ asset($image->image) }}" class="img-fluid" width="100">
+                            </a>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
